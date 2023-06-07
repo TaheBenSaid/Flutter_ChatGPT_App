64c5041030b14592fa2c6a2e47dff33d5a34e413
Tahe <taha.bensaid132002@gmail.com>
2023-06-07 11:19:37 +0100

    final commit

--- lib/chat_message.dart
+++ lib/chat_message.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class ChatMessage extends StatelessWidget {
-  const ChatMessage({Key? key, required this.text, required this.sender})
-      : super(key: key);
+  const ChatMessage(
+      {super.key,
+        required this.text,
+        required this.sender,
+        this.isImage = false});
 
   final String text;
   final String sender;
+  final bool isImage;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          margin: EdgeInsets.only(right: 16.0),
-          child: CircleAvatar(
-            child: Text(sender[0]),
-          ),
-        ),
+        Text(sender)
+            .text
+            .subtitle1(context)
+            .make()
+            .box
+            .color(sender == "user" ? Vx.red200 : Vx.green200)
+            .p16
+            .rounded
+            .alignCenter
+            .makeCentered(),
         Expanded(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(sender, style: Theme.of(context).textTheme.subtitle1),
-            Container(
-              margin: const EdgeInsets.only(top: 5.0),
-              child: Text(text),
-            )
-          ],
-        ))
+          child: isImage
+              ? AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Image.network(
+              text,
+              loadingBuilder: (context, child, loadingProgress) =>
+              loadingProgress == null
+                  ? child
+                  : const CircularProgressIndicator.adaptive(),
+            ),
+          )
+              : text.trim().text.bodyText1(context).make().px8(),
+        ),
       ],
-    );
+    ).py8();
   }
 }
--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,7 +1,10 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import 'chat_message.dart';
+import 'threedots.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -12,36 +15,103 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
-  final List<ChatMessage> _message = [];
+  final List<ChatMessage> _messages = [];
+  late OpenAI chatGPT;
+  bool _isImageSearch = false;
+  bool _isTyping = false;
 
-  // This function is called when the user presses the send button
-  void _sendMessage() {
-    ChatMessage _message = ChatMessage(
-      text: _controller.text,
-      sender: "User",
+  @override
+  void initState() {
+    chatGPT = OpenAI(
+      apiKey: dotenv.env["YOUR_API_KEY"],
+      language: Language.english,
     );
-    //
-    // setState(() {
-    //   _message.insert(0, _message); // Append the message to the list
-    // });
-
-    _controller.clear(); // Clear the text field
+    super.initState();
   }
 
-  Widget _buildComposer() {
+  @override
+  void dispose() {
+    chatGPT.close();
+    super.dispose();
+  }
+
+  void _sendMessage() async {
+    if (_controller.text.isEmpty) return;
+    ChatMessage message = ChatMessage(
+      text: _controller.text,
+      sender: "user",
+      isImage: false,
+    );
+
+    setState(() {
+      _messages.insert(0, message);
+      _isTyping = true;
+    });
+
+    _controller.clear();
+
+    if (_isImageSearch) {
+      final response = await chatGPT.generateImages(
+        [message.text],
+        height: 256,
+        width: 256,
+        topK: 1,
+      );
+      Vx.log(response.data!.last.url!);
+      insertNewData(response.data!.last.url!, isImage: true);
+    } else {
+      final response = await chatGPT.complete(
+        message.text,
+        maxTokens: 100,
+      );
+      Vx.log(response.choices[0].text);
+      insertNewData(response.choices[0].text, isImage: false);
+    }
+  }
+
+  void insertNewData(String response, {bool isImage = false}) {
+    ChatMessage botMessage = ChatMessage(
+      text: response,
+      sender: "bot",
+      isImage: isImage,
+    );
+
+    setState(() {
+      _isTyping = false;
+      _messages.insert(0, botMessage);
+    });
+  }
+
+  Widget _buildTextComposer() {
     return Row(
       children: [
         Expanded(
           child: TextField(
             controller: _controller,
-            onSubmitted: (value) => _sendMessage(), // Send the message
-
-            decoration:
-                const InputDecoration.collapsed(hintText: "Send a message"),
+            onSubmitted: (value) => _sendMessage(),
+            decoration: const InputDecoration.collapsed(
+              hintText: "Question/description",
+            ),
           ),
         ),
-        IconButton(
-            onPressed: () => _sendMessage(), icon: const Icon(Icons.send))
+        ButtonBar(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                _isImageSearch = false;
+                _sendMessage();
+              },
+            ),
+            TextButton(
+              onPressed: () {
+                _isImageSearch = true;
+                _sendMessage();
+              },
+              child: const Text("Generate Image"),
+            )
+          ],
+        ),
       ],
     ).px16();
   }
@@ -49,24 +119,29 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("ChatGPT App"),
-      ),
+      appBar: AppBar(title: const Text("ChatGPT & Dall-E Demo")),
       body: SafeArea(
         child: Column(
           children: [
             Flexible(
-                child: ListView.builder(
-              reverse: true,
-              padding: Vx.m8,
-              itemCount: _message.length,
-              itemBuilder: (context, index) {
-                return _message[index];
-              },
-            )),
+              child: ListView.builder(
+                reverse: true,
+                padding: Vx.m8,
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return _messages[index];
+                },
+              ),
+            ),
+            if (_isTyping) const ThreeDots(),
+            const Divider(
+              height: 1.0,
+            ),
             Container(
-              decoration: BoxDecoration(color: context.cardColor),
-              child: _buildComposer(),
+              decoration: BoxDecoration(
+                color: context.cardColor,
+              ),
+              child: _buildTextComposer(),
             )
           ],
         ),
